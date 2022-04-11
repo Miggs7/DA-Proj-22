1f0d6816f433c366018643d6c4a39f8bbb001cd8
PedradasDaTuga <pedromiguelcorreia2002@gmail.com>
2022-04-11 20:01:38 +0100

    Cenario 1 Pedro

--- Project/main.cpp
+++ Project/main.cpp
@@ -38,13 +38,16 @@ public:
     EstafetaouPedido(int pe,int v){
         vol=v;
         peso=pe;
+
     }
+
     int const getVol(){
         return vol;
     }
     int const getPeso(){
         return peso;
     }
+
 };
 class Carrinha{
     int volMax;
@@ -57,6 +60,8 @@ public:
         volMax = v;
         pesoMax = p;
         custo = c;
+        volatual=v;
+        pesoatual=p;
     }
     int getVolMax(){
         return volMax;
@@ -74,10 +79,10 @@ public:
         return pesoatual;
     }
     void setPesoAtual(int addedpeso){
-        this->pesoatual+=addedpeso;
+        pesoatual=pesoatual-addedpeso;
     }
     void setVolAtual(int addedvol){
-        this->volatual+=addedvol;
+        volatual=volatual-addedvol;
     }
 };
 
@@ -86,46 +91,51 @@ bool compararCarrinhas(Carrinha a, Carrinha b){
 }
 
 bool compararEnc(Encomenda a, Encomenda b){
-    return((a.getPeso()+a.getVol()+a.getDurar())<=(b.getPeso()+b.getVol()+b.getDurar()));
+    return((a.getPeso()+a.getVol())<=(b.getPeso()+b.getVol()));
 }
 
-int preenchertruck(Carrinha truck,Encomenda encos,int time){
+int preenchertruck(Carrinha& truck,Encomenda& encos){
     int tempV = encos.getVol();
     int tempW = encos.getPeso();
 
-    if((tempV+truck.getVolatual() > truck.getVolMax() && tempW+truck.getPesoAtual() > truck.getPesoMax()) || time > 86400 ){ // casos base
+   /* if(((tempV+truck.getVolatual()) > truck.getVolMax() || (tempW+truck.getPesoAtual() )> truck.getPesoMax())){ // casos base
         return 0;
     }
-
-    if(tempV+truck.getVolatual()<= truck.getVolMax() && tempW+truck.getPesoAtual() <= truck.getPesoMax() && time < 86400){
-        truck.setPesoAtual(tempW+truck.getPesoAtual());
-        truck.setVolAtual(tempV+truck.getVolatual());
+*/
+    if((truck.getVolatual()-tempV)>=0 && (truck.getPesoAtual()-tempW)>=0){
+        truck.setPesoAtual(tempW);
+        truck.setVolAtual(tempV);
+        return 1;
     }
 
-    return 1;
+    return 0;
 }
 
 
-int escolhertruck(vector<Encomenda>& encos, vector<Carrinha>& trucks,vector<EstafetaouPedido>& pedidos,vector<EstafetaouPedido>& estafeta ){
+int escolhertruck(vector<Encomenda>& encos, vector<Carrinha>& trucks,vector<EstafetaouPedido>& pedidos,vector<EstafetaouPedido>& estafetas ){
     int count = 0;
     int t = 0;
+    int flag=0;
+
     for(auto truck : trucks) {
-       // t=0;
-        if(encos.empty()) break;
-        if(t>=86400)
+       flag=0;
+        if(encos.empty())
             break;
         for(int i = 0; i < (int) encos.size(); i++){
-            if(preenchertruck(truck,encos.at(i),t) == 1){
-                t += encos.at(i).getDurar();
-                EstafetaouPedido encomendausada(encos.at(i).getPeso(),encos.at(i).getVol());
-                pedidos.push_back(encomendausada);
-                encos.erase(encos.begin()+i);
-                i--;
+                if(preenchertruck(truck,encos.at(i)) == 1){
+                   EstafetaouPedido encomendausada(encos.at(i).getPeso(),encos.at(i).getVol()); //Serve para inserir nos pedidos
+                    pedidos.push_back(encomendausada);
+                    encos.erase(encos.begin()+i);
+                    i--;
+                    flag=1;
+                }
             }
+        if(flag==1){
+            EstafetaouPedido temp(truck.getPesoMax(),truck.getVolMax()); //Serve para inserir nas estafetas
+            estafetas.push_back(temp);
+            count++;
+            continue;
         }
-        EstafetaouPedido temp(truck.getPesoMax(),truck.getVolMax());
-        estafeta.push_back(temp);
-        count++;
     }
     return count;
 }
@@ -136,8 +146,8 @@ int main() {
     string carrinhasfile("carrinhas.txt");
     string line;
 
-    vector<Encomenda> enco;
-    vector<Carrinha> truck;
+    vector<Encomenda> encos;
+    vector<Carrinha> trucks;
     ifstream input_file(encomendasfile);
 
     if(!input_file.is_open()){
@@ -153,7 +163,7 @@ int main() {
         int duracao = atoi(strtok(NULL," "));
         free(dup);
         Encomenda temp(vol,peso,recompensa,duracao);
-        enco.push_back(temp);
+        encos.push_back(temp);
     }
 
     //leitura carrinhas
@@ -167,27 +177,37 @@ int main() {
         int custo  = atoi(strtok(NULL," "));
         free(dup);
         Carrinha temp2(volMax,pesoMax,custo);
-        truck.push_back(temp2);
+        trucks.push_back(temp2);
     }
 
-    std::sort(truck.begin(),truck.end(), compararCarrinhas);
-    std::sort(enco.begin(),enco.end(), compararEnc);
+    std::sort(trucks.begin(),trucks.end(), compararCarrinhas);
+    std::sort(encos.begin(),encos.end(), compararEnc);
     int sum=0;
-    for (int i = 0; i < enco.size(); i++) {
-        sum+=enco.at(i).getDurar();
+    for (int i = 0; i < encos.size(); i++) {
+        sum+=encos.at(i).getDurar();
         }
     cout<<"TEmpo total: "<<sum<<endl;
     vector<EstafetaouPedido> Estafetas;
     vector<EstafetaouPedido> Pedidos;
-    cout<<"Encos "<<enco[0].getPeso()<<endl;
-    cout << escolhertruck(enco,truck,Pedidos,Estafetas) << endl;
+    cout << escolhertruck(encos,trucks,Pedidos,Estafetas) << endl;
+    int pp=0;
+    int vv=0;
     for (int i=0; i<Estafetas.size();i++){
-        cout<<"Estafeta "<<i+1<<" Peso:"<<Estafetas.at(i).getPeso()<<" Vol:"<<Estafetas.at(i).getVol()<<endl;
+        pp+=Estafetas.at(i).getPeso();
+        vv+=Estafetas.at(i).getVol();
     }
+    cout<<"Estafetas PesoTotal " <<pp<<" Vol total:"<<vv<<endl;
     int te=0;
+    pp=0;
+    vv=0;
+    int tempo=0;
    for (int i = 0; i < Pedidos.size(); i++) {
+       pp+=Pedidos.at(i).getPeso();
+       vv+=Pedidos.at(i).getVol();
         te++;
+
     }
-    cout<<"Pedido "<<te<<endl;
+    cout<<"Nº Pedido "<<te<<" Peso total:"<<pp<<" Vol total:"<<vv
+    <<" TEmpo"<<tempo<<endl;
     return 0;
 }
